2022e2de75dceb3f6817056bd3dc431497cbca4b
didu <hanhanjsw@163.com>
2020-12-20 06:15:20 +0800

    修改题库相关接口

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/microservices/knowledgemap/class
+make build-linux
+cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/microservices/knowledgemap/passport
+make build-linux
+cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/microservices/knowledgemap/user
+make build-linux
+cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/microservices/knowledgemap/question
+make build-linux
+cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/microservices/knowledgemap/knowledgemap
+make build-linux
