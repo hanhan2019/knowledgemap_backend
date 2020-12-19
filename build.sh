d8d7e928e0e883d65e03894d15a9f9f40c0687f4
didu <hanhanjsw@163.com>
2020-12-20 06:34:30 +0800

    修改题库相关接口

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/microservices/knowl
 make build-linux
 cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/microservices/knowledgemap/knowledgemap
 make build-linux
+cd /Users/firewinggames/code/gopath/src/knowledgemap_backend/agent/knowledgemap
+env GOOS=linux GOARCH=amd64 go build -o agent-linux cmd/knowledgemap.go
